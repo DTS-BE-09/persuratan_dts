385bc8b066e521bebca584b81ab43526aaa0bc38
rifarihadts <rifariha.dts@gmail.com>
2020-10-20 20:35:00 +0700

    check user condition

--- controllers/suratController.js
+++ controllers/suratController.js
@@ -4,23 +4,32 @@ import jwt from 'jsonwebtoken'
 import bodyParser from 'body-parser'
 import path from 'path'
 import fileUpload from 'express-fileupload'
+import fs from 'fs'
 
 const __dirname = path.resolve();
-
 const suratRouter = express.Router()
 
-userRouter.use(bodyParser.urlencoded({extended: false}));
-userRouter.use(bodyParser.json());
+suratRouter.use(bodyParser.urlencoded({extended: false}));
+suratRouter.use(bodyParser.json());
 suratRouter.use(fileUpload());
 
+var dir = './uploads';
+
+//buat folder untuk upload jika belum ada
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
+//api untuk upload surat
 suratRouter.post('/upload-surat', async (req, res) => {
     const authHeader = req.headers.authorization;
-
+    var userdata = [];
     if (authHeader) {
         const token = authHeader.split(' ')[1];
 
         jwt.verify(token, 'secret', (err, user) => {
-            const jabatanAllowed = [1,2]
+            userdata = user
+            const jabatanAllowed = [1,2] //cek jika user dengan kode jabatan yang di define tidak ada maka return eror
             if (!jabatanAllowed.includes(user.jabatan)) 
                 return res.status(403).json({"message": "Anda tidak memiliki wewenang"});
             if (err) {
@@ -45,8 +54,34 @@ suratRouter.post('/upload-surat', async (req, res) => {
                 return res.status(500).send(err);
         });
 
-        const newSurat = new Surat({"surat": filename})
-        const createdSurat = await newSurat.save()
+        //jika jabatan nya adalah pegawai biasa
+        if(userdata.jabatan == 1)
+        {
+            const newSurat = new Surat(
+                {
+                    "user_id":userdata.id,
+                    "surat": filename,
+                    "feedback_spv" : false,
+                    "feedback_spv_time":null,
+                    "feedback_bos" : false,
+                    "feedback_bos_time":null,
+                })
+            await newSurat.save()
+        }
+        else //jika jabatan yang mengupload adalah supervisor maka kolom persetujuan spv langsung otomatis true 
+        {
+            const newSurat = new Surat(
+                {
+                    "user_id":userdata.id,
+                    "surat": filename,
+                    "feedback_spv" : true,
+                    "feedback_bos" : false,
+                    "feedback_bos_time":null,
+                }
+            )
+            await newSurat.save()
+        }
+        
         res.status(200).send({"message": "File berhasil diupload"});
     } catch (error) {
         console.log(error)
--- models/surat.js
+++ models/surat.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
 
 const suratSchema = mongoose.Schema({
+    user_id : {
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
     surat : {
         type:String,
         required:true
