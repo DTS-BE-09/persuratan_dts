c88cefcc9feeb9f88fc476289fbabedbb433ab55
imadedwisuryadinata <dwi.suryadinata@gmail.com>
2020-10-24 21:17:49 +0800

    mengganti validasi token dan menambah fitur surat

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,6 +3,7 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import bodyParser from 'body-parser'
+import Conf from '../config.js'
 
 const adminRouter = express.Router()
 
@@ -11,144 +12,135 @@ adminRouter.use(bodyParser.json());
 
 // add new user
 adminRouter.post('/register', async (req, res) => {
-    try {
 
-        const authHeader = req.headers.authorization;
+    
+    //header apabila akan melakukan akses
+    var token = req.headers.authorization;
+        if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+        
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+          const jabatan = user.jabatan;
+          if (jabatan == '0'){
+            try {
 
-        if (authHeader) {
-            const token = authHeader.split(' ')[1];
-            jwt.verify(token, 'secret', (err, user) => {
-                if (user.jabatan != 0) 
-                    return res.status(403).json({"message": "Anda tidak memiliki wewenang"});
-                if (err) {
-                    console.log(err)
-                    return res.sendStatus(403);
-                }
-            });
+                const {name, email, password, jabatan} = req.body
+
+                var saltRounds = 10;
+                const hashedPw = await bcrypt.hash(password, saltRounds)
+                const newUser = new User({"name": name, "email": email, "password": hashedPw, "jabatan": jabatan})
+                const createdUser = await newUser.save()
+                res.status(201).json(createdUser)
+            } catch (error) {
+                console.log(error)
+                res.status(500).json({error: error})
+            }
         } else {
-            res.sendStatus(401);
+            res.status(201).json({"status":"Anda tidak berwenang melakukan registrasi!"})
         }
 
-        const {username, email, password, jabatan} = req.body
-
-        var saltRounds = 10;
-        const hashedPw = await bcrypt.hash(password, saltRounds)
-        const newUser = new User({"username": username, "email": email, "password": hashedPw, "jabatan": jabatan})
-        const createdUser = await newUser.save()
-        res.status(201).json(createdUser)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({error: error})
-    }
+    })
 })
 
 // get all user
 adminRouter.get('/', async (req, res) => {
-    const authHeader = req.headers.authorization;
-
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-
-        jwt.verify(token, 'secret', (err, user) => {
-            if (user.jabatan != 0) 
-                return res.status(403).json({"message": "Anda tidak memiliki wewenang"});
-            
-            if (err) {
-                console.log(err)
-                return res.sendStatus(403);
-            }
-        });
-    } else {
-        res.sendStatus(401);
-    }
-
-    const user = await User.find({})
-
-    if (user && user.length !== 0) {
+    //header apabila akan melakukan akses
+    var token = req.headers.authorization;
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      const jabatan = user.jabatan;
+      if (jabatan == '0'){
+        const user = await User.find({});
         res.json(user)
-    } else {
-        res.status(404).json({message: 'User not found'})
-    }
+      } else {
+        res.status(201).json({"status":"Anda tidak berwenang melihat daftar user"})
+      }
+
+    })
 })
 
 // get user by id
 adminRouter.get('/:id', async (req, res) => {
-    const authHeader = req.headers.authorization;
-
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, 'secret', (err, user) => {
-            if (user.jabatan != 0) 
-                return res.status(403).json({"message": "Anda tidak memiliki wewenang"});
-            
-            if (err) {
-                console.log(err)
-                return res.sendStatus(403);
-            }
-        });
-    } else {
-        res.sendStatus(401);
-    }
-
-    try {
-        const user = await new Promise((resolve, reject) => {
-            User.findById(req.params.id, (err, user) => {
-                if (err) 
-                    reject(err)
-                resolve(user)
+ 
+    //header apabila akan melakukan akses
+    var token = req.headers.authorization;
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      const jabatan = user.jabatan;
+      if (jabatan == '0'){
+        try {
+            const user = await new Promise((resolve, reject) => {
+                User.findById(req.params.id, (err, user) => {
+                    if (err) 
+                        reject(err)
+                    resolve(user)
+                })
             })
-        })
-        res.json(user)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
+            res.json(user)
+        } catch (error) {
+            res.status(500).json({error: error})
+        }
+    } else {
+        res.status(201).json({"status":"Anda tidak berwenang melihat daftar user"})
+      }
+
+    })
+
 })
 
 // update user data
 adminRouter.patch('/:id', async (req, res) => {
-    const authHeader = req.headers.authorization;
+    
+    //header apabila akan melakukan akses
+    var token = req.headers.authorization;
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //verifikasi jwt
+    jwt.verify(token, Conf.secret, async(err, user) => {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      const jabatan = user.jabatan;
+      if (jabatan == '0'){
+        try {
+            const { name, email, password, jabatan } = req.body;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
+            const user = await User.findById(req.params.id);
 
-        jwt.verify(token, 'secret', (err, user) => {
-            if (user.jabatan != 0) 
-                return res.status(403).json({"message": "Anda tidak memiliki wewenang"});
-            if (err) {
-                console.log(err)
-                return res.sendStatus(403);
+            if(user){
+                
+                user.name= name;
+                user.email= email;
+                var saltRounds = 10;
+                const hashedPw = await bcrypt.hash(password, saltRounds);
+                user.password = hashedPw;
+                user.jabatan = jabatan
+
+                const updatedUser = await user.save();
+
+                res.json(updatedUser);
+            } else {
+                res.status(404).json({
+                    message: 'user not found'
+                })
             }
-        });
-    } else {
-        res.sendStatus(401);
-    }
 
-    var ObjectId = require('mongodb').ObjectID
-    try {
-        const user = await new Promise((resolve, reject) => {
-            User.findById(req.params.id, (err, user) => {
-                if (err) 
-                    reject(err)
-                resolve(user)
-            })
-        })
-        const data = req.body
-
-        if (data.hasOwnProperty('password')) {
-            var saltRounds = 10;
-            const hashedPw = await bcrypt.hash(data.password, saltRounds)
-            data.password = hashedPw
+    
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({"status": "user not found"});
         }
 
-        await User.update({
-            _id: ObjectId(req.params.id)
-        }, {$set: data});
-        res.status(201).json({"status": "Update success!"});
+      } else {
+        res.status(201).json({"status":"Anda tidak berwenang untuk update user"})
+      }
 
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({"status": "user not found"});
-    }
+    })
 })
 
 export default adminRouter
